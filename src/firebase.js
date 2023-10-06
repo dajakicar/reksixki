1ab754edbeaea615a4289633a20d719dc820b312
dajakicar <147117024+dajakicar@users.noreply.github.com>
2023-10-06 03:27:35 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -15,13 +15,13 @@ import { getFirestore } from "firebase/firestore";
 // };
 
 const firebaseConfig = {
-   apiKey: "AIzaSyCOmTnd8Md2yK0SVEwI1l6VbV1M7N8ySaA",
-   authDomain: "magneti1001.firebaseapp.com",
-   projectId: "magneti1001",
-   storageBucket: "magneti1001.appspot.com",
-   messagingSenderId: "596941070186",
-   appId: "1:596941070186:web:e9af3e1e5ecb7459e80f13",
-   measurementId: "G-Z02PPN0HRS"
+  apiKey: "AIzaSyCdQGpKAt_GhX6sPVku872TX0X6euU3RL4",
+  authDomain: "loadmp3z.firebaseapp.com",
+  projectId: "loadmp3z",
+  storageBucket: "loadmp3z.appspot.com",
+  messagingSenderId: "247165037086",
+  appId: "1:247165037086:web:8ee2968672de6bfdde5c0f",
+  measurementId: "G-CR50YZS9EF"
 };
 
 // Initialize Firebase
